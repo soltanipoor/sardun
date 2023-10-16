aa6e768e4a4933c03faaa6aa2f24dec8c8c61a25
Ali Soltani <soltanipoor1@gmail.com>
2023-10-16 19:19:53 +0330

    button component

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom";
+import Button from "../components/UI/Button";
 
 const AuthPage = () => {
   return (
-    <div>
+    <div className="px-16">
       <Link to="/">صفحه اول</Link>
       <h1>ورود / عضویت</h1>
+
+      <div className="flex gap-4 items-center">
+        <Button variant="fill" size="md">ورود</Button>
+        <Button variant="outline" size="md">ورود</Button>
+
+        <Button variant="fill" size="sm">ورود</Button>
+        <Button variant="outline" size="sm">ورود</Button>
+      </div>
     </div>
   );
 };
